63c2f6712013d850d1678eab046f6d7196371ee5
Dovydas Rupsys <dovydas.rupsys@cryptohaven.com>
2018-08-27 17:33:38 +0100

    Add contain value assertions

--- src/Predicates/Map.php
+++ src/Predicates/Map.php
@@ -47,7 +47,7 @@ trait Map
     {
         $isStrictModeDisabled = $wanted[1] === true;
         $strict = $isStrictModeDisabled ? ' :strict' : '';
-        $assertion = contains($this->actual, $wanted[0], !$isStrictModeDisabled);
+        $assertion = contains(array_keys($this->actual), $wanted[0], !$isStrictModeDisabled);
         $passed = $this->hasAssertionFailed($assertion);
         $positive = $this->positive ? 'contain' : 'not contain';
 
@@ -101,11 +101,11 @@ trait Map
         $isStrictModeDisabled = $wanted[1] === true;
         $strict = $isStrictModeDisabled ? ' :strict' : '';
         $notFound = reduce_left($wanted[0], function($value, $key, $c, $carry) use ($isStrictModeDisabled) {
-            return reject($carry, function ($wanted) use ($value, $isStrictModeDisabled) {
+            return reject($carry, function ($actualKeys) use ($value, $isStrictModeDisabled) {
                 if ($isStrictModeDisabled) {
-                    return $wanted == $value;
+                    return $actualKeys == $value;
                 } else {
-                    return $wanted === $value;
+                    return $actualKeys === $value;
                 }
             });
         }, array_keys($this->actual));
@@ -123,13 +123,83 @@ trait Map
         );
     }
 
+    public function containValue(array $wanted): Output
+    {
+        $isStrictModeDisabled = $wanted[1] === true;
+        $strict = $isStrictModeDisabled ? ' :strict' : '';
+        $assertion = contains(array_values($this->actual), $wanted[0], !$isStrictModeDisabled);
+        $passed = $this->hasAssertionFailed($assertion);
+        $positive = $this->positive ? 'contain' : 'not contain';
+
+        $value = $wanted[0];
+        if (is_bool($value)) {
+            $value = $value ? 'true' : 'false';
+        } elseif (is_object($value)) {
+            $value = json_encode($value);
+        }
+
+        return new Output(
+            $this->getStatus($passed),
+            new Text("Expected :actual to ${positive} value :value${strict}", Color::PRIMARY),
+            [
+                "actual" => new Text(json_encode($this->actual), Color::FAILURE),
+                "value" => new Text($value, Color::SUCCESS),
+                'strict' => new Text('[NOT STRICT]', Color::WARNING),
+            ]
+        );
+    }
+
     public function containValues(array $wanted): Output
     {
+        $isStrictModeDisabled = $wanted[1] === true;
+        $strict = $isStrictModeDisabled ? ' :strict' : '';
+        $notFound = reduce_left($this->actual, function($value, $key, $c, $carry) use ($isStrictModeDisabled) {
+            return reject($carry, function ($wanted) use ($value, $isStrictModeDisabled) {
+                if ($isStrictModeDisabled) {
+                    return $wanted == $value;
+                } else {
+                    return $wanted === $value;
+                }
+            });
+        }, $wanted[0]);
 
+        $passed = $this->hasAssertionFailed(empty($notFound));
+        $positive = $this->positive ? 'contain' : 'not contain';
+        return new Output(
+            $this->getStatus($passed),
+            new Text("Expected :actual to ${positive} values :values${strict}", Color::PRIMARY),
+            [
+                "actual" => new Text(json_encode($this->actual), Color::FAILURE),
+                "values" => new Text(json_encode($wanted[0]), Color::SUCCESS),
+                'strict' => new Text('[NOT STRICT]', Color::WARNING),
+            ]
+        );
     }
 
     public function containOnlyValues(array $wanted): Output
     {
+        $isStrictModeDisabled = $wanted[1] === true;
+        $strict = $isStrictModeDisabled ? ' :strict' : '';
+        $notFound = reduce_left($wanted[0], function($value, $key, $c, $carry) use ($isStrictModeDisabled) {
+            return reject($carry, function ($actualValues) use ($value, $isStrictModeDisabled) {
+                if ($isStrictModeDisabled) {
+                    return $actualValues == $value;
+                } else {
+                    return $actualValues === $value;
+                }
+            });
+        }, array_values($this->actual));
 
+        $passed = $this->hasAssertionFailed(empty($notFound));
+        $positive = $this->positive ? 'contain' : 'not contain';
+        return new Output(
+            $this->getStatus($passed),
+            new Text("Expected :actual to ${positive} only values :values${strict}", Color::PRIMARY),
+            [
+                "actual" => new Text(json_encode($this->actual), Color::FAILURE),
+                "values" => new Text(json_encode($wanted[0]), Color::SUCCESS),
+                'strict' => new Text('[NOT STRICT]', Color::WARNING),
+            ]
+        );
     }
 }
