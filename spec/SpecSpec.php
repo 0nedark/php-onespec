ce5203718994220974d0260bc4ebccf60962977f
Dovydas Rupsys <dovydas.rupsys@cryptohaven.com>
2018-09-05 23:42:41 +0100

    Add test for the duplicate describe assertions

--- spec/SpecSpec.php
+++ spec/SpecSpec.php
@@ -8,15 +8,12 @@ use Xae3Oow5cahz9shahngu\Spec as Mock;
 
 $spec = Spec::class(__FILE__,Mock::class);
 
-$spec->before(function (ClassBuilder $obj) {
-    $obj->beConstructedWith('hash', 'name', new ClassBuilder(\DateTimeZone::class));
-});
-
 $spec->describe("name hashing", function(Spec $spec) {
     $spec->it("should not throw exception if test name is in a different describe", function ($expect, ClassBuilder $obj) {
-        $expect(function () use ($obj) {
-            $obj->describe('name hashing', function (Spec $spec) {
-                $spec->describe('name hashing', function (Spec $spec) {});
+        $expect(function () {
+            $obj = Mock::class('file', \DateTimeZone::class);
+            $obj->describe('name hashing', function ($spec) {
+                $spec->describe('name hashing', function ($spec) {});
             });
         })->toNotThrowException();
     });
